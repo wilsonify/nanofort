73d59c3a68f1417eec38b84d602d4ec290103feb
wilsonify <tom.a.wilson@gmail.com>
2021-05-15 15:15:41 -0400

    create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pipeline',
+    version='',
+    packages=['pipeline'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
